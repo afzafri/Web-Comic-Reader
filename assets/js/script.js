230779abebc8d579b7ade65a2a98a2bbd5966988
afzafri <zafri_21596@yahoo.com>
2017-04-08 09:21:41 +0800

    use uncompress.js function to open the archive

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,17 @@ $(document).ready(function(){
     $("#fileup").change(function(){
         var file = $(this)[0].files[0];
         
-        console.log(file);
+        // Open the file as an archive
+        archiveOpenFile(file, function(archive, err) {
+            if (archive) 
+            {
+               console.log(archive);
+            } 
+            else 
+            {
+                console.log(err);
+            }
+        });
         
     });
     
