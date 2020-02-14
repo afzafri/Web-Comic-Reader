1287b8192f0ba112d68b85e5a407f6ceee9faa8a
Afif Zafri <afif@redant.my>
2020-02-14 12:10:05 +0800

    Added feature to load comic from internal file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,109 @@
 $(document).ready(function(){
-	
+
 	// current year
 	$('#currYear').html((new Date()).getFullYear());
-   
+
     // Load all the archive formats
-	loadArchiveFormats(['rar', 'zip', 'tar']);
-    
+		loadArchiveFormats(['rar', 'zip', 'tar']);
+
+		// ----- OPEN COMIC FROM COMPUTER -----
     $("#fileup").change(function(){
-		
-		if($(this)[0].files.length == 0) {
-			$('#output').html("<font color='red'>Please choose a comic file</font><br>");
-		} else {
-			$('#output').hide();
-			
-			// init the gallery plugin, when there is a first click on a image
-			// re-bind this function when opening new comic
-			$(document).one('click','#comicImg',function(){
-				event.preventDefault();
-				// initialize gallery
-				$('#output').lightGallery({
-					selector: 'a',
-					zoom: true,
-					fullScreen: true,
-					download: false,
-					enableTouch: true,
-				});
-				$(this).click();
-			});
-			
-			// Update progress text
-			$('.progress-text').html("Reading 0/0 pages");
-			
-			// show loading
-			$('.se-pre-con').fadeIn('slow');
-			
-			// destroy lightGallery
-			var $lg = $('#output');
-			$lg.lightGallery();
-			$lg.data('lightGallery').destroy(true);
-			
-			// clear previous blobs
-			clearBlobs();
-			
-			// clear previous output data
-			$('#output').empty();
-			
-			var file = $(this)[0].files[0];
-			
-			// Open the file as an archive
-			archiveOpenFile(file, function(archive, err) {
-				if (archive) 
-				{
-					$('#output').append("<b>"+archive.file_name+"</b><br><i>Click on the image to enlarge</i><br><br>");
-					readContents(archive);
-				} 
-				else 
-				{
-					$('#output').append("<font color='red'>"+err+"</font><br>");
-					
-					// hide loading
-					$('.se-pre-con').fadeOut('slow');
-					
-					// show output box
-					$('#output').fadeIn('slow');
-				}
-			});
-		}
+			if($(this)[0].files.length == 0) {
+				$('#output').html("<font color='red'>Please choose a comic file</font><br>");
+			} else {
+				var file = $(this)[0].files[0];
+				// open the comic
+				openComic(file);
+			}
     });
-    
+
+		// ----- OPEN COMIC FROM INTERNAL FILE IN SERVER -----
+		$(document).on('click', '#readNow', function() {
+			// get the comic file name
+			var comictitle = $(this).attr('comic_title');
+
+			var blob = null;
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", "/comics/"+comictitle); // make sure to put all the comics inside "comics" directory in the root directory
+			xhr.responseType = "blob";
+			xhr.onload = function()
+			{
+			    blob = xhr.response;
+					var file = new File([blob], comictitle);
+					// open the comic
+					openComic(file);
+			}
+			xhr.send();
+		});
+
+		function openComic(file)
+		{
+				$('#output').hide();
+
+				// init the gallery plugin, when there is a first click on a image
+				// re-bind this function when opening new comic
+				$(document).one('click','#comicImg',function(){
+					event.preventDefault();
+					// initialize gallery
+					$('#output').lightGallery({
+						selector: 'a',
+						zoom: true,
+						fullScreen: true,
+						download: false,
+						enableTouch: true,
+					});
+					$(this).click();
+				});
+
+				// Update progress text
+				$('.progress-text').html("Reading 0/0 pages");
+
+				// show loading
+				$('.se-pre-con').fadeIn('slow');
+
+				// destroy lightGallery
+				var $lg = $('#output');
+				$lg.lightGallery();
+				$lg.data('lightGallery').destroy(true);
+
+				// clear previous blobs
+				clearBlobs();
+
+				// clear previous output data
+				$('#output').empty();
+
+				// Open the file as an archive
+				archiveOpenFile(file, function(archive, err) {
+					if (archive)
+					{
+						$('#output').append("<b>"+archive.file_name+"</b><br><i>Click on the image to enlarge</i><br><br>");
+						readContents(archive);
+					}
+					else
+					{
+						$('#output').append("<font color='red'>"+err+"</font><br>");
+
+						// hide loading
+						$('.se-pre-con').fadeOut('slow');
+
+						// show output box
+						$('#output').fadeIn('slow');
+					}
+				});
+		}
+
     // function for reading the contents of the archive
     function readContents(archive)
     {
         var entries = archive.entries;
-        
+
         // iterate through all the contents
         for(var i=0;i<entries.length;i++)
-        {	
+        {
 			processEntries(entries, i, entries.length);
         }
     }
-	
+
 	// implement setTimout()
 	// This can minimize browser from overprocessing and end up freezing
 	// simple way to mimics multi threading
@@ -95,20 +119,20 @@ $(document).ready(function(){
 			}
 		}, i*700);
 	}
-    
+
     // function to return file extension based on file name
     function getExt(filename)
     {
         var ext = filename.split('.').pop();
         return (ext == filename) ? '' : ext;
     }
-    
+
     // function to return MIME type based on the file extension
     // NOTE: THIS FUNCTION IS NOT EFFICIENT
     function getMIME(filename)
     {
         var ext = getExt(filename).toLowerCase();
-        
+
         switch(ext)
         {
             case 'jpg':
@@ -131,7 +155,7 @@ $(document).ready(function(){
                 return 'image/jpeg';
         }
     }
-    
+
     // function to convert the archive contents into blobs, and return URL
     function createBlobs(entry, i, max)
     {
@@ -139,26 +163,26 @@ $(document).ready(function(){
             // Convert the data into an Object URL
             var blob = new Blob([data], {type: getMIME(entry.name)});
             var url = URL.createObjectURL(blob);
-            
+
             // output the images
             $('#output').append("<a href='"+url+"' id='comicImg'><img src='"+url+"' class='imgUrl'/></a>");
-			
+
 			// Update progress text
 			$('.progress-text').html("Reading "+i+"/"+max+" pages");
-			
+
 			// only hide loading spinnder when done process all
 			if(i == (max-1)) {
 				$('.progress-text').html("<font color='lime'>Completed!</font>");
-				
+
 				// hide loading
                 $('.se-pre-con').fadeOut('slow');
-                
+
                 // show output box
                 $('#output').fadeIn('slow');
 			}
-        }); 
+        });
     }
-    
+
     // function to clear all previous blobs, free up memory
     function clearBlobs()
     {
@@ -166,5 +190,5 @@ $(document).ready(function(){
             URL.revokeObjectURL($(this).attr('src'));
         });
     }
-    
+
 });
