c0d55827a3c000913840cfa6174446671fc08471
Afif Zafri <afif@redant.my>
2023-08-30 11:15:20 +0800

    Read file and create blob asynchronously, improve performance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 
-		// tab switch
-		$(document).on('click', '#tab', function() {
-			// reset all
-			$('#output').hide();
-			$('.tab-button').removeClass('active');
-			$('.option').hide();
+	// tab switch
+	$(document).on('click', '#tab', function() {
+		// reset all
+		$('#output').hide();
+		$('.tab-button').removeClass('active');
+		$('.option').hide();
 
-			// set active
-			var currentID = $(this).attr('tab');
-			$(this).addClass('active');
-			$('#'+currentID).show();
-		});
+		// set active
+		var currentID = $(this).attr('tab');
+		$(this).addClass('active');
+		$('#'+currentID).show();
+	});
 
-		// current year
-		$('#currYear').html((new Date()).getFullYear());
+	// current year
+	$('#currYear').html((new Date()).getFullYear());
 
-    // Load all the archive formats
-		loadArchiveFormats(['rar', 'zip', 'tar']);
+	// Load all the archive formats
+	loadArchiveFormats(['rar', 'zip', 'tar']);
 
-		// ----- OPEN COMIC FROM COMPUTER -----
-    $("#fileup").change(function(){
+	// ----- OPEN COMIC FROM COMPUTER -----
+	$("#fileup").change(function(){
 			if($(this)[0].files.length == 0) {
 				$('#output').html("<font color='red'>Please choose a comic file</font><br>");
 			} else {
@@ -28,119 +28,135 @@ $(document).ready(function(){
 				// open the comic
 				openComic(file);
 			}
-    });
+	});
 
-		// ----- OPEN COMIC FROM INTERNAL FILE IN SERVER -----
-		$(document).on('click', '.readNow', function() {
-			// get the comic file name
-			var comictitle = $(this).attr('comic_title');
+	// ----- OPEN COMIC FROM INTERNAL FILE IN SERVER -----
+	$(document).on('click', '.readNow', function() {
+		// get the comic file name
+		var comictitle = $(this).attr('comic_title');
 
-			// disable loading other comic while loading
-			toggleReadNow();
+		// disable loading other comic while loading
+		toggleReadNow();
 
-			var blob = null;
-			var xhr = new XMLHttpRequest();
-			xhr.open("GET", "./comics/"+comictitle); // make sure to put all the comics inside "comics" directory in the root directory
-			xhr.responseType = "blob";
-			xhr.onload = function()
-			{
-			    blob = xhr.response;
-					var file = new File([blob], comictitle);
-					// open the comic
-					openComic(file);
-			}
-			xhr.send();
-		});
-
-		function toggleReadNow(disable = true) {
-			$('.readNow').prop('disabled', disable);
-		}
-
-		function openComic(file)
+		var blob = null;
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "./comics/"+comictitle); // make sure to put all the comics inside "comics" directory in the root directory
+		xhr.responseType = "blob";
+		xhr.onload = function()
 		{
-				$('#output').hide();
+			blob = xhr.response;
+				var file = new File([blob], comictitle);
+				// open the comic
+				openComic(file);
+		}
+		xhr.send();
+	});
 
-				// init the gallery plugin, when there is a first click on a image
-				// re-bind this function when opening new comic
-				$(document).one('click','#comicImg',function(){
-					event.preventDefault();
-					// initialize gallery
-					$('#output').lightGallery({
-						selector: 'a',
-						zoom: true,
-						fullScreen: true,
-						download: false,
-						enableTouch: true,
-					});
-					$(this).click();
+	function toggleReadNow(disable = true) {
+		$('.readNow').prop('disabled', disable);
+	}
+
+	function openComic(file)
+	{
+			$('#output').hide();
+
+			// init the gallery plugin, when there is a first click on a image
+			// re-bind this function when opening new comic
+			$(document).one('click','#comicImg',function(){
+				event.preventDefault();
+				// initialize gallery
+				$('#output').lightGallery({
+					selector: 'a',
+					zoom: true,
+					fullScreen: true,
+					download: false,
+					enableTouch: true,
 				});
+				$(this).click();
+			});
 
-				// Update progress text
-				$('.progress-text').html("Reading 0/0 pages");
+			// Update progress text
+			$('.progress-text').html("Reading 0/0 pages");
 
-				// show loading
-				$('.se-pre-con').fadeIn('slow');
+			// show loading
+			$('.se-pre-con').fadeIn('slow');
 
-				// destroy lightGallery
-				var $lg = $('#output');
-				$lg.lightGallery();
-				$lg.data('lightGallery').destroy(true);
+			// destroy lightGallery
+			var $lg = $('#output');
+			$lg.lightGallery();
+			$lg.data('lightGallery').destroy(true);
 
-				// clear previous blobs
-				clearBlobs();
+			// clear previous blobs
+			clearBlobs();
 
-				// clear previous output data
-				$('#output').empty();
+			// clear previous output data
+			$('#output').empty();
 
-				// Open the file as an archive
-				archiveOpenFile(file, function(archive, err) {
-					if (archive)
-					{
-						$('#output').append("<b>"+archive.file_name+"</b><br><i>Click on the image to enlarge</i><br><br>");
-						readContents(archive);
-					}
-					else
-					{
-						$('#output').append("<font color='red'>"+err+"</font><br>");
+			// Open the file as an archive
+			archiveOpenFile(file, function(archive, err) {
+				if (archive)
+				{
+					$('#output').append("<b>"+archive.file_name+"</b><br><i>Click on the image to enlarge</i><br><br>");
+					readContents(archive);
+				}
+				else
+				{
+					$('#output').append("<font color='red'>"+err+"</font><br>");
 
-						// hide loading
-						$('.se-pre-con').fadeOut('slow');
+					// hide loading
+					$('.se-pre-con').fadeOut('slow');
 
-						// show output box
-						$('#output').fadeIn('slow');
-						
-						// re-enable read now
-						toggleReadNow(false);
-					}
-				});
+					// show output box
+					$('#output').fadeIn('slow');
+					
+					// re-enable read now
+					toggleReadNow(false);
+				}
+			});
+	}
+
+	// Function to read the contents of the archive
+	async function readContents(archive) {
+		var entries = archive.entries;
+
+		// Create an array to hold the promises
+		var promises = [];
+
+		// Loop through all the contents
+		for (var i = 0; i < entries.length; i++) {
+			var filename = entries[i].name;
+			// Check if it's an output file and not a folder
+			if (getExt(filename) !== '') {
+				// Push the promise to the array
+				promises.push(createBlobAsync(entries[i], i, entries.length));
+			}
 		}
 
-    // function for reading the contents of the archive
-    function readContents(archive)
-    {
-        var entries = archive.entries;
+		// Wait for all promises to resolve
+		await Promise.all(promises);
+	}
 
-        // iterate through all the contents
-        for(var i=0;i<entries.length;i++)
-        {
-			processEntries(entries, i, entries.length);
-        }
-    }
+	// function to convert the archive contents into blobs asynchronously, and return URL
+	function createBlobAsync(entry, i, max) {
+		return new Promise((resolve) => {
+			entry.readData(function(data, err) {
+				var blob = new Blob([data], { type: getMIME(entry.name) });
+				var url = URL.createObjectURL(blob);
 
-	// implement setTimout()
-	// This can minimize browser from overprocessing and end up freezing
-	// simple way to mimics multi threading
-	function processEntries(entries, i, max)
-	{
-		setTimeout(function() {
-			filename = entries[i].name;
-			// check, only output file, not folder
-			if(getExt(filename) != '')
-			{
-				// call function to create blob and url. allow us to read/view the contents
-				createBlobs(entries[i], i, max);
-			}
-		}, i*700);
+				$('#output').append("<a href='" + url + "' id='comicImg'><img src='" + url + "' class='imgUrl'/></a>");
+
+				$('.progress-text').html("Reading " + (i + 1) + "/" + max + " pages");
+
+				if (i === max - 1) {
+					$('.progress-text').html("<font color='lime'>Completed!</font>");
+					$('.se-pre-con').fadeOut('slow'); // hide loading
+					$('#output').fadeIn('slow'); // show output box
+					toggleReadNow(false); // re-enable read now
+				}
+
+				resolve(); // Resolve the promise
+			});
+		});
 	}
 
     // function to return file extension based on file name
@@ -152,62 +168,21 @@ $(document).ready(function(){
 
     // function to return MIME type based on the file extension
     // NOTE: THIS FUNCTION IS NOT EFFICIENT
-    function getMIME(filename)
-    {
-        var ext = getExt(filename).toLowerCase();
+    function getMIME(filename) 
+	{
+		var ext = getExt(filename).toLowerCase();
 
-        switch(ext)
-        {
-            case 'jpg':
-            case 'jpeg':
-                return 'image/jpeg';
-                break;
-            case 'png':
-                return 'image/png';
-                break;
-            case 'gif':
-                return 'image/gif';
-                break;
-            case 'bmp':
-                return 'image/bmp';
-                break;
-            case 'webp':
-                return 'image/webp';
-                break;
-            default:
-                return 'image/jpeg';
-        }
-    }
+		var mimeTypes = {
+			'jpg': 'image/jpeg',
+			'jpeg': 'image/jpeg',
+			'png': 'image/png',
+			'gif': 'image/gif',
+			'bmp': 'image/bmp',
+			'webp': 'image/webp'
+		};
 
-    // function to convert the archive contents into blobs, and return URL
-    function createBlobs(entry, i, max)
-    {
-        entry.readData(function(data, err) {
-            // Convert the data into an Object URL
-            var blob = new Blob([data], {type: getMIME(entry.name)});
-            var url = URL.createObjectURL(blob);
-
-            // output the images
-            $('#output').append("<a href='"+url+"' id='comicImg'><img src='"+url+"' class='imgUrl'/></a>");
-
-			// Update progress text
-			$('.progress-text').html("Reading "+i+"/"+max+" pages");
-
-			// only hide loading spinnder when done process all
-			if(i == (max-1)) {
-				$('.progress-text').html("<font color='lime'>Completed!</font>");
-
-				// hide loading
-                $('.se-pre-con').fadeOut('slow');
-
-                // show output box
-                $('#output').fadeIn('slow');
-
-				// re-enable read now
-				toggleReadNow(false);
-			}
-        });
-    }
+		return mimeTypes[ext] || 'image/jpeg'; // Default to JPEG if extension is not recognized
+	}
 
     // function to clear all previous blobs, free up memory
     function clearBlobs()
