053a31668fa09a44a4fc1bc54345e0029a86e474
Afif Zafri <afif@redant.my>
2025-10-30 14:50:13 +0800

    remove read comic from server methods, new collapsible uploaded container

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const outputElement = document.getElementById('output');
     const progressTextElement = document.querySelector('.progress-text');
     const sePreConElement = document.querySelector('.se-pre-con');
-    const readNowButtons = document.querySelectorAll('.readNow');
     const currYearElement = document.getElementById('currYear');
-    const tabButtons = document.querySelectorAll('#tab');
-    const allTabButtons = document.querySelectorAll('.tab-button');
-    const allOptions = document.querySelectorAll('.option');
-
-    // tab switch
-    tabButtons.forEach(tab => {
-        tab.addEventListener('click', () => {
-            // reset all
-            outputElement.style.display = 'none';
-            allTabButtons.forEach(button => button.classList.remove('active'));
-            allOptions.forEach(option => option.style.display = 'none');
-
-            // set active
-            const currentID = tab.getAttribute('tab');
-            tab.classList.add('active');
-            document.getElementById(currentID).style.display = 'block';
-        });
-    });
+    const wrapElement = document.querySelector('.wrap');
 
     // current year
     currYearElement.innerHTML = (new Date()).getFullYear();
@@ -30,78 +12,49 @@ document.addEventListener('DOMContentLoaded', () => {
     // Load all the archive formats
     loadArchiveFormats(['rar', 'zip', 'tar']);
 
-    // ----- OPEN COMIC FROM COMPUTER -----
-    let dropzone = new Dropzone("div#dropzone", {
-        url: '#', // Dummy URL, not used since you're not uploading files
-        dictDefaultMessage: 'Click or Drop files here to upload <br> <i>(cbr,cbz,cbt files only)</i>',
-        autoProcessQueue: false, // Disable automatic uploads
-        disablePreviews: false,
-        createImageThumbnails: false,
+    // click on collapsed footer to expand uploader
+    document.querySelector('.footer-collapsed').addEventListener('click', () => {
+        wrapElement.classList.remove('collapsed');
+    });
+
+    // Dropzone configuration
+    if (window.Dropzone) Dropzone.autoDiscover = false;
+    let dropzone = new Dropzone("#dropzone", {
+        url: '#',
         acceptedFiles: '.cbr,.cbz,.cbt',
+        createImageThumbnails: false,
+        autoProcessQueue: false,
+        previewsContainer: false,
         maxFiles: 1,
         maxfilesexceeded: function(file) {
             this.removeAllFiles();
         },
         init: function () {
             this.on('addedfile', function (file) {
-                // Handle the dropped file here
                 openComic(file);
             });
         }
     });
 
-    // ----- OPEN COMIC FROM INTERNAL FILE IN SERVER -----
-    readNowButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            // get the comic file name
-            const comictitle = button.getAttribute('comic_title');
-
-            // disable loading other comic while loading
-            toggleReadNow();
-
-            fetch("./comics/" + comictitle)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error("Network response was not ok " + response.statusText);
-                    }
-                    return response.blob();
-                })
-                .then(blob => {
-                    const file = new File([blob], comictitle);
-                    // open the comic
-                    openComic(file);
-                })
-                .catch(error => {
-                    console.error("There was a problem with the fetch operation:", error);
-                    toggleReadNow(false); // re-enable the button if there is an error
-                });
-        });
-    });
-
-    function toggleReadNow(disable = true) {
-        readNowButtons.forEach(button => {
-            button.disabled = disable;
-        });
-    }
-
     function openComic(file) {
         outputElement.style.display = 'none';
+        wrapElement.classList.add('collapsed');
 
-		// init the gallery plugin, when there is a first click on a image
-		// re-bind this function when opening new comic
-		// TODO: remove jquery code. Dependency: lightGallery
-		$(document).one('click','#comicImg',function(event){
-			event.preventDefault();
-			// initialize gallery
-			$('#output').lightGallery({
-				selector: 'a',
-				zoom: true,
-				fullScreen: true,
-				download: false,
-				enableTouch: true,
-			});
-			$(this).click();
-		});
+        // init the gallery plugin, when there is a first click on a image
+        // re-bind this function when opening new comic
+        // TODO: remove jquery code. Dependency: lightGallery
+        $(document).one('click','#comicImg',function(event){
+            event.preventDefault();
+            // initialize gallery
+            $('#output').lightGallery({
+                selector: 'a',
+                zoom: true,
+                fullScreen: true,
+                download: false,
+                enableTouch: true,
+            });
+            $(this).click();
+        });
 
         // Update progress text
         progressTextElement.innerHTML = "Reading 0/0 pages";
@@ -110,10 +63,10 @@ document.addEventListener('DOMContentLoaded', () => {
         sePreConElement.style.display = 'block';
 
         // destroy lightGallery
-		// TODO: remove jquery code. Dependency: lightGallery
+        // TODO: remove jquery code. Dependency: lightGallery
         var $lg = $('#output');
-		$lg.lightGallery();
-		$lg.data('lightGallery').destroy(true);
+        $lg.lightGallery();
+        $lg.data('lightGallery').destroy(true);
 
         // clear previous blobs
         clearBlobs();
@@ -127,16 +80,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 outputElement.innerHTML = `<b>${archive.file_name}</b><br><i>Click on the image to enlarge</i><br><br>`;
                 readContents(archive);
             } else {
-                outputElement.innerHTML = `<font color='red'>${err}</font><br>`;
+                outputElement.innerHTML = `<span style="color: #ef4444;">${err}</span><br>`;
 
                 // hide loading
                 sePreConElement.style.display = 'none';
 
                 // show output box
                 outputElement.style.display = 'block';
-                
-                // re-enable read now
-                toggleReadNow(false);
             }
         });
     }
@@ -175,10 +125,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 progressTextElement.innerHTML = `Reading ${i + 1}/${max} pages`;
 
                 if (i === max - 1) {
-                    progressTextElement.innerHTML = "<font color='lime'>Completed!</font>";
+                    progressTextElement.innerHTML = '<span style="color: #4ade80;">Completed!</span>';
                     sePreConElement.style.display = 'none';
                     outputElement.style.display = 'block';
-                    toggleReadNow(false);
                 }
 
                 resolve();
